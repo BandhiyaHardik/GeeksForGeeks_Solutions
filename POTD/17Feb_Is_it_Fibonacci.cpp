f4897a7afe06908d207077fd181dae05d85d9fa4
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-02-17 22:34:52 +0530

    GeeksForGeeks_POTD_Solution

---
+++ POTD/17Feb_Is_it_Fibonacci.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    long long solve(int N, int K, vector<long long> GeekNum) {
+        long long next_term=0;
+        for(int i=0;i<K; i++)
+        {
+            if(i==N-1)
+            {
+                return GeekNum[i];
+            }
+            next_term+=GeekNum[i];
+        }
+        GeekNum.push_back(next_term);
+        
+        int i=0;
+        int j=K;
+        
+        while(j!=N)
+        {
+            next_term-=GeekNum[i];
+            next_term+=GeekNum[j];
+            GeekNum.push_back(next_term);
+            i++;
+            j++;
+        }
+        
+        return GeekNum[N-1];
+    }
+};
