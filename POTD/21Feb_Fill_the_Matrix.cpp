2e4acae11ce12f734e1b0e428a116f874a95ce1d
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-02-21 22:46:49 +0530

    GeeksForGeeks

---
+++ POTD/21Feb_Fill_the_Matrix.cpp
@@ -0,0 +1,15 @@
+class Solution{   
+public:
+    int minIteration(int N, int M, int x, int y){    
+        int ans=0;
+        for(int i=1; i<=N; i++)
+        {
+            for(int j=1; j<=M; j++)
+            {
+                ans=max(ans,abs(i-x)+abs(j-y));
+            }
+        }
+        
+        return ans;
+    }
+};
