19e34997dc60f66bbeb023c1616c7fa76b62d4f9
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-03-29 22:12:35 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/29Mar_Count_the_Substrings.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+    int countSubstring(string S)
+    {
+        int ans=0;
+        for(int i=0;i<S.size(); i++)
+        {
+            int low=0;
+            int upp=0;
+            for(int j=i; j<S.size(); j++)
+            {
+                if(S[j]>='a' and S[j]<='z')
+                {
+                    low++;
+                }
+                if(S[j]>='A' and S[j]<='Z')
+                {
+                    upp++;
+                }
+                if(low==upp)
+                {
+                    ans++;
+                }
+            }
+            
+        }
+        
+        return ans;
+    }
+};
