6b0986425cecd822cc5256d0ecc398d46b06ab6a
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-07-12 22:13:05 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/12July_Power_Of_Numbers.cpp
@@ -0,0 +1,21 @@
+class Solution{
+    public:
+    
+    long long power(long long N, long long R)
+    {
+        const int mod = 1e9 + 7;
+        long long ans = 1;
+        
+        while(R){
+            if(R & 1)
+                ans = (ans * N) % mod;
+                
+            N = (N * N) % mod; 
+                
+            R >>= 1;
+        }
+        
+        return ans;
+    }
+
+};
