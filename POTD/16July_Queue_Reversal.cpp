c61749dda15cb7b6072b9e28a310a1b411ba9cbc
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-07-16 22:53:18 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/16July_Queue_Reversal.cpp
@@ -0,0 +1,22 @@
+//Function to reverse the queue.
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+          helper(q);
+     	return q;
+    }
+    
+     void helper(queue<int>& q)
+    {
+        if (q.size() == 0)
+            return ;
+
+        int x = q.front();
+        q.pop();
+        helper(q);
+        q.push(x);
+    }
+    
+};
