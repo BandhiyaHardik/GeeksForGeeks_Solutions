6bae66c05d82da6b46d0398f3649e3bc2350bcd7
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-09-14 23:36:11 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/14Sep_Perfect_Sum_Problem.cpp
@@ -0,0 +1,20 @@
+class Solution{
+const int mod = 1000000007;
+	public:
+int perfectSum(int arr[], int n, int sum) {
+    int dp[n + 1][sum + 1];
+
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= sum; j++) {
+            if (i == 0)
+                dp[i][j] = (j == 0) ? 1 : 0;
+            else if (j < arr[i - 1])
+                dp[i][j] = dp[i - 1][j];
+            else
+                dp[i][j] = (dp[i - 1][j] + dp[i - 1][j - arr[i - 1]]) % mod;
+        }
+    }
+
+    return dp[n][sum] % mod;
+}
+};
