e1388e627c9802c56048c46f65f1555cba0b8ca0
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-10-09 22:26:28 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/09Oct_Height_of_Binary_Tree.cpp
@@ -0,0 +1,11 @@
+class Solution{
+    public:
+    //Function to find the height of a binary tree.
+    int height(struct Node* node){
+         if(node==nullptr)
+        return 0;
+        
+        return max(height(node->left), height(node->right))+1;
+        
+    }
+};
