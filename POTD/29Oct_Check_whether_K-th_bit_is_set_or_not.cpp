a987f391988b6817e7308822a28c9d84e2c6ccdb
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-10-30 00:54:49 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/29Oct_Check_whether_K-th_bit_is_set_or_not.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        
+        if( (n&(1<<(k))) != 0)
+        return true;
+        
+        return false;
+    }
+};
