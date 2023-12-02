ff06f1cea025fa4e219c3d7f6c42e37a0f777421
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2023-12-02 23:16:07 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD/02Dec_Inorder_Traversal_and_BST.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int isRepresentingBST(int arr[], int N)
+    {
+        for(int i = 1; i < N; i++){
+            if(arr[i] <= arr[i - 1])
+                return 0;
+        }
+        
+        return 1;
+    }
+};
