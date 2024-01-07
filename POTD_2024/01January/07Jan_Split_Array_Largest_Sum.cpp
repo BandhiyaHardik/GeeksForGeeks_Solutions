0838614c9b0080d9fe84c8e89c235128c2baaadc
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-01-07 23:43:12 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/01January/07Jan_Split_Array_Largest_Sum.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    int splitArray(int arr[] ,int N, int K) {
+        int low = 0;
+        int high = 0;
+        
+        for(int i = 0; i < N; i++)
+            high += arr[i], low = max(low, arr[i] - 1);
+            
+        while(low < high - 1){
+            int mid = low + (high - low) / 2;
+            
+            auto check = [&]() -> bool {
+                int cuts = 0;
+                
+                int sum = 0;
+                
+                for(int i = 0; i < N; i++){
+                    if(sum + arr[i] > mid){
+                        ++cuts;
+                        sum = arr[i];
+                    }
+                    else{
+                        sum += arr[i];
+                    }
+                }
+                
+                return cuts < K;
+            };
+            
+            if(check())
+                high = mid;
+            else
+                low = mid;
+        }
+        
+        return high;
+    }
+};
