cf5fc239d11b91ff775e65e5ce6569d0df7d05d1
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-02-23 22:54:30 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/02February/23Feb_Buy_and_Sell_a_Share_at_most_twice.cpp
@@ -0,0 +1,25 @@
+class Solution
+{
+    public:
+        //User function Template for C++
+        
+        int maxProfit(vector<int>&price){
+            
+            int n=price.size();
+            vector<int>temp(n,0);
+
+            int maxi=price[n-1];
+            for(int i=n-2;i>=0;i--){
+                maxi=max(maxi, price[i]);
+                temp[i]=max(temp[i+1], maxi-price[i]);
+            }
+            
+            int mini=price[0];
+            for(int i=1;i<n;i++){
+                mini=min(price[i],mini);
+                temp[i]=max(temp[i-1], price[i]-mini+temp[i]);
+            }
+            
+            return temp[n-1];
+        }
+};
