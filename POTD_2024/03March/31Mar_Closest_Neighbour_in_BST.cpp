82e2fe1d2710285ccec29f050d32589ab235435a
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-03-31 16:31:29 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/03March/31Mar_Closest_Neighbour_in_BST.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int ans=INT_MIN;
+    
+    void solve(Node* root, int n){
+        if(root==NULL) return;
+        
+        if(root->key <= n){
+            ans=max(ans, root->key);
+            solve(root->right, n);
+        }
+        else{
+            solve(root->left, n);
+        }
+        
+        return;
+    }
+    int findMaxForN(Node* root, int n) {
+        solve(root, n);
+        if(ans==INT_MIN){
+            ans=-1;
+        }
+        
+        return ans;
+        
+    }
+};
