f063dfe39b2f8cab1bf78eacf1084fea660dcd58
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-04-13 23:18:06 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/04April/13Apr_Reverse_Bits.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    long long reversedBits(long long x) {
+        string s="";
+        for(int i=0;i<=31;i++){
+            if(x & (1<<i)){
+                s='1'+s;
+            }
+            else{
+                s='0'+s;
+            }
+        }
+        
+        long long ans=0;
+        for(int i=0;i<=31;i++){
+            if(s[i]=='1'){
+                ans+=pow(2,i);
+            }
+        }
+        return ans;
+    }
+};
