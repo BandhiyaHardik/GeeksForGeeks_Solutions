94d91bcdac3b99d78e9b086ad7196984cf5a12fc
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-04-25 23:11:22 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/04April/25Apr_Maximum_sum_of_hour_glass.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int findMaxSum(int n, int m, vector<vector<int>> mat) {
+        
+        if(n<3 || m<3){
+            return -1;
+        }
+        int ans=-1;
+        
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<m-2;j++){
+
+                int sum=mat[i][j]+mat[i][j+1]+mat[i][j+2]+mat[i+1][j+1]+mat[i+2][j]+mat[i+2][j+1]+mat[i+2][j+2];
+                
+                ans=max(ans, sum);
+            }
+        }
+        return ans;
+    }
+};
