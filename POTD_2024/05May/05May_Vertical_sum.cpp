d11c794391f48bb76fd5f1b3be57654f641f8319
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-05-05 22:58:32 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/05May/05May_Vertical_sum.cpp
@@ -0,0 +1,23 @@
+class Solution{
+  public:
+  map<int,int>mp;
+  void preorder(Node* node, int level){
+      if(node!=NULL){
+          mp[level]+=node->data;
+          
+          preorder(node->left, level-1);
+          preorder(node->right, level+1);
+          
+          return;
+      }
+  }
+  
+    vector <int> verticalSum(Node *root) {
+        preorder(root, 0);
+        vector<int>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
