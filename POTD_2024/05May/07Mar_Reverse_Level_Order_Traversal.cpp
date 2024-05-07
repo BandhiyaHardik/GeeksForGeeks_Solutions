3ba10c25cc1799de180ecae2f918d4f44fd718fd
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-05-07 22:49:46 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/05May/07Mar_Reverse_Level_Order_Traversal.cpp
@@ -0,0 +1,25 @@
+vector<int> reverseLevelOrder(Node *root)
+{
+    vector<int>ans;
+    stack<int>st;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        Node* node=q.front(); q.pop();
+        st.push(node->data);
+        
+        if(node->right){
+            q.push(node->right);
+        }
+        if(node->left){
+            q.push(node->left);
+        }
+    }
+    
+    while(!st.empty()){
+        ans.push_back(st.top());
+        st.pop();
+    }
+    
+    return ans;
+}
