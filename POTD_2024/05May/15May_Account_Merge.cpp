88948e6e651c1f1f3a6e04c4f4253ac5dc7c733c
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-05-15 23:56:33 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/05May/15May_Account_Merge.cpp
@@ -0,0 +1,46 @@
+class Solution{
+public:
+  string find(string s, map<string, string> &p)
+  {
+    return p[s] == s ? s : find(p[s], p);
+  }
+  
+  vector<vector<string>> accountsMerge(vector<vector<string>> &accounts)
+  {
+
+    map<string, string> owner;
+    map<string, string> parents;
+    map<string, set<string>> unions;
+
+    for (int i = 0; i < accounts.size(); i++)
+    {
+
+      for (int j = 1; j < accounts[i].size(); j++)
+      {
+        parents[accounts[i][j]] = accounts[i][j];
+        owner[accounts[i][j]] = accounts[i][0];
+      }
+    }
+
+    for (int i = 0; i < accounts.size(); i++)
+    {
+      string p = find(accounts[i][1], parents);
+      for (int j = 2; j < accounts[i].size(); j++)
+        parents[find(accounts[i][j], parents)] = p;
+    }
+
+    for (int i = 0; i < accounts.size(); i++)
+      for (int j = 1; j < accounts[i].size(); j++)
+        unions[find(accounts[i][j], parents)].insert(accounts[i][j]);
+
+    vector<vector<string>> ans;
+    for (pair<string, set<string>> p : unions)
+    {
+      vector<string> res(p.second.begin(), p.second.end());
+      res.insert(res.begin(), owner[p.first]);
+      ans.push_back(res);
+    }
+
+    return ans;
+  }
+};
