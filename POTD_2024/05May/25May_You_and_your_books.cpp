73f7f35e1b074a3c19a37d52fa77f9d72292cdbf
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-05-25 23:13:12 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/05May/25May_You_and_your_books.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int arr[], int n, int k) {
+        long long ans=0, curr=0;
+        int i=0;
+        while(i<n){
+            if(arr[i]<=k){
+                curr+=arr[i];
+            }
+            else{
+                curr=0;
+            }
+            
+            ans=max(ans, curr);
+            i++;
+        }
+        return ans;
+    }
+};
