1d18d614ca522de9839799e4e555945d3a782a9f
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-05-30 18:21:07 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/05May/30May_String_Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int mod = 1e9+7;
+    int help(string a,string b,int i,int j,vector<vector<int>> &dp){
+        if(j==b.length())return 1;
+        if(i==a.length())return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int x=0,y=0;
+        if(a[i]==b[j])x=help(a,b,i+1,j+1,dp);
+        y=help(a,b,i+1,j,dp);
+        return dp[i][j] = (x+y)%mod;
+    }
+    int countWays(string s1, string s2) {
+
+        vector<vector<int>> dp(s1.length(),vector<int> (s2.length(),-1));
+        return help(s1,s2,0,0,dp);
+    }
+};
