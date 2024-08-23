0ed925591705788d8025376f65bf9e7ba1711433
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-08-23 23:21:33 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/08August/23August_Left_View_of_Binary_Tree.cpp
@@ -0,0 +1,14 @@
+void solve(Node *root,vector<int> &ans,int level){
+    if(!root) return;
+    
+    if(ans.size()==level)
+    ans.push_back(root->data);
+    solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
+}
+vector<int> leftView(Node *root)
+{
+   vector<int> ans;
+   solve(root,ans,0);
+   return ans;
+}
