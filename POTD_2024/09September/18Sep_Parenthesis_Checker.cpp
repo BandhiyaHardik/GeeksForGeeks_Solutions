90bd1731df9babaf955455c9e7e60beb55d18b9f
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-09-18 22:53:58 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/09September/18Sep_Parenthesis_Checker.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    public:
+    //Function to check if brackets are balanced or not.
+    bool ispar(string x)
+    {
+        unordered_map<char,char> mm;
+        mm[')']='(';
+        mm[']']='[';
+        mm['}']='{';
+        stack<char> st;
+        for(auto c:x){
+            if(c=='(' or c=='[' or c=='{')st.push(c);
+            else if(st.empty())return false;
+            else{
+                if(st.top()!=mm[c])return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+
+};
