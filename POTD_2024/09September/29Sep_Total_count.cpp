6eec57d95d0e3dc14f61efa8a886c4a229bb6d16
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-09-29 23:35:05 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/09September/29Sep_Total_count.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int totalCount(int k, vector<int>& arr) {
+
+        int ans = 0;
+        for(auto x : arr) {
+            ans += (x + k - 1) / k;
+        }
+        return ans;
+    }
+};
