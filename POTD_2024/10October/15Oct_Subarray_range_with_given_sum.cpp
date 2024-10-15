836f99ea4991825f520e78bcb3dde1637e9129df
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-10-15 23:54:35 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/10October/15Oct_Subarray_range_with_given_sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+
+        unordered_map<int,int> mm;
+        int ans=0,sum=0;
+        for(int x:arr){
+            sum+=x;
+            if(sum==tar)ans++;
+            if(mm.find(sum - tar) != mm.end())ans+=mm[sum-tar];
+            mm[sum]++;
+        }
+        return ans;
+    }
+};
