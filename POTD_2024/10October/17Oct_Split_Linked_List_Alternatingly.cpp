22d4ae1fc1f6ca2ccfb91d6ecf9bc63e562fef42
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-10-17 23:44:37 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/10October/17Oct_Split_Linked_List_Alternatingly.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    // Function to split a linked list into two lists alternately
+     vector<Node*> alternatingSplitList(struct Node* head) {
+
+        Node *head1=NULL,*head2=NULL,*temp1=NULL,*temp2=NULL;
+        int count=0;
+        while(head){
+            if(count%2){
+                if(!head2){
+                    head2=head;
+                    temp2=head;
+                }
+                else{
+                    temp2->next=head;
+                    temp2=temp2->next;
+                }
+            }
+            else{
+                if(!head1){
+                    head1=head;
+                    temp1=head;
+                }
+                else{
+                    temp1->next=head;
+                    temp1=temp1->next;
+                }
+            }
+            head=head->next;
+            count++;
+        }
+        temp1->next=NULL;
+        temp2->next=NULL;
+        return {head1,head2};
+    }
+
+};
