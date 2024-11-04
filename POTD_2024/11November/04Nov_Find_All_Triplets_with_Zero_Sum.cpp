a7b8b56230983f8d653d6249f965d6016cd77045
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2024-11-04 21:53:32 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2024/11November/04Nov_Find_All_Triplets_with_Zero_Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &arr) {
+
+        vector<vector<int>> ans;
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            int a = arr[i];
+            for(int j=i+1; j<n; j++){
+                int b = arr[j];
+                for(int k=j+1; k<n; k++){
+                    int c  = arr[k];
+                    if(a+b+c == 0){
+                        ans.push_back({i, j, k});
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
