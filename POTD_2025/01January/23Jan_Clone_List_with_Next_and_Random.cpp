f6ffed25dda719470f85ca2d060ddd6902d50a6b
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-01-23 22:20:05 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/01January/23Jan_Clone_List_with_Next_and_Random.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+    Node *cloneLinkedList(Node *head) {
+
+        if(head==NULL) return head;
+        Node * temp=head;
+        while(temp){
+            Node *n=new Node(temp->data);
+            n->next=temp->next;
+            temp->next=n;
+            temp=n->next;
+        }
+
+        temp=head;
+        Node *head2=temp->next;
+        while(temp!=NULL){
+            if(temp->random==NULL){
+                temp->next->random=NULL;
+            }
+            else
+            temp->next->random=temp->random->next;
+            temp=temp->next->next;
+        }
+
+        temp = head;
+        while(temp!=NULL){
+            Node * temp2=temp->next;
+            temp->next=temp2->next;
+            if(temp2->next)
+            temp2->next=temp2->next->next;
+            temp=temp->next;
+        }
+        return head2;
+    }
+};
