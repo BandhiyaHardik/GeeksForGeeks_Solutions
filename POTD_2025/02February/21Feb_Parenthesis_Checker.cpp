4b6d3ae228dd1eb65a95c24c2836c29c60db20c0
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-02-21 23:46:37 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/02February/21Feb_Parenthesis_Checker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(' || s[i]=='[' || s[i]=='{') st.push(s[i]);
+            else{
+                if(!st.empty() && ((st.top()=='(' && s[i]==')') || (st.top()=='[' && s[i]==']')||
+                (st.top()=='{' && s[i]=='}'))) st.pop();
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
