044d61c040bca4866a395f78d75f21ab1a177f93
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-05-10 23:53:05 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/05May/10May_Longest_Subarray_with_Majority_Greater_than_K.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int longestSubarray(vector<int> &arr, int k) {
+
+        unordered_map<int,int>mp;
+        mp[0]=-1;
+        int prefixsum=0,maxlen=0;
+        for(int i=0;i<arr.size();i++){
+            prefixsum+=(arr[i]>k)?1:-1;
+            if(prefixsum>0) maxlen=i+1;
+            
+            if(mp.count(prefixsum-1)){
+                maxlen=max(maxlen,i-mp[prefixsum-1]);
+            }
+            
+            if(!mp.count(prefixsum)) mp[prefixsum]=i;
+        }
+        return maxlen;
+    }
+};
