3e552dc0cdb5747057b8b8a9d3b332e124d01482
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-05-11 23:56:21 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/05May/11May_K_th_Largest_Sum_Contiguous_Subarray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+
+        int n=arr.size();
+        vector<int>pre(n+1,0);
+        
+        for(int i=0;i<n;i++) pre[i+1]=pre[i]+arr[i];
+        priority_queue<int,vector<int>,greater<int>>mH;
+        
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<=n;j++){
+                int sum= pre[j]-pre[i];
+                if(mH.size()<k) mH.push(sum);
+                else if(sum>mH.top()){
+                    mH.pop();
+                    mH.push(sum);
+                }
+             }
+        }
+        return mH.top();
+    }
+};
