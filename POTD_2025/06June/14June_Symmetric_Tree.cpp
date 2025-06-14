2000b07d11977146be5018a2b67e5081ed13b137
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-06-14 19:57:29 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/06June/14June_Symmetric_Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public: 
+    bool solve(Node* left, Node* right){
+        if(left==NULL || right==NULL) return left==right;
+        if(left->data != right->data) return false;
+        return solve(left->left,right->right) && solve(left->right,right->left);
+    }
+    bool isSymmetric(Node* root) {
+
+        Node* left=root->left;
+        Node* right=root->right;
+        return solve(left,right);
+        
+    }
+};
