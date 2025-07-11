e34caf13b80b0aaad3ec3037f1dc028e66322d44
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-07-11 23:48:18 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/07July/11July_Trail_of_ones.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int countConsec(int n) {
+
+        if(n==2) return 1;
+        int prev=0;
+        int curr=1;
+        int next;
+        int ans=1;
+        for(int i=3;i<=n;i++){
+            next=prev+curr;
+            ans=ans*2+ next;
+            prev=curr;
+            curr=next;
+        }
+        return ans;
+    }
+};
