eee344ccfda78517a63a28fcec4b8bc83b55a982
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-08-09 19:46:25 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/08August/09Aug_Longest_Periodic_Proper_Prefix.cpp
@@ -0,0 +1,38 @@
+class Solution {
+  public:
+    
+    vector<int> zFunction(const string &s) {
+        int n = s.size();
+        vector<int> z(n, 0);
+    
+        int l = 0, r = 0;
+        for (int i = 1; i < n; ++i) {
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+    
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                z[i]++;
+            }
+    
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+    
+    int getLongestPrefix(const string &s) {
+        int n = s.size();
+        vector<int> z = zFunction(s);
+    
+        for (int i = n - 1; i > 0; i--) {
+            if (z[i] == n - i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+};
