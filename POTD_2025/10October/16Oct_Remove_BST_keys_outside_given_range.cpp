c367c0028ca89783e5eebbb465e420614aeda68d
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-10-17 00:26:10 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/10October/16Oct_Remove_BST_keys_outside_given_range.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    Node* removekeys(Node* root, int l, int r) {
+      if(!root)
+        return NULL;
+      root->left = removekeys(root->left, l, r);
+      root->right = removekeys(root->right, l, r);
+      int data = root->data;
+      if(data < l)
+        return root->right;
+      if(data > r)
+        return root->left;
+      return root;          
+    }
+};
