28aae7f04b4385af5f00cc152264b62104dc943b
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-10-26 23:20:29 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/10October/26Oct_Minimum_Cost_of_ropes.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int minCost(vector<int>& arr) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+        for(int &a:arr) {
+            pq.push(a);
+        }
+        int cost = 0;
+        while(pq.size() > 1) {
+            int f = pq.top();
+            pq.pop();
+            int s = pq.top();
+            pq.pop();
+            cost += (f+s);
+            pq.push(f+s);
+        }
+        return cost;
+    }
+};
