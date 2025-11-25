beccede0806bcd178f9cd5c456fd3ceb36e31045
HardikBandhiya <bandhiya.hardik1905@gmail.com>
2025-11-25 21:06:46 +0530

    "GeeksForGeeks_POTD_Solutions"

---
+++ POTD_2025/11November/25Nov_Game_of_XOR.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    int subarrayXor(vector<int>& a) {
+        int ans=a[0];
+        if(a.size()%2==0)return 0;
+        for(int i=2;i<a.size();i+=2){
+        ans^=a[i];
+        }
+        return ans;
+    }
+};
